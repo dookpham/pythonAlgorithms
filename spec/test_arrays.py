37e35bd312a697b9a9680b039ca26d63c21e50b6
DookPham <dookpham@gmail.com>
2016-09-16 13:14:52 -0700

    Implement lambda functions in tests

--- spec/test_arrays.py
+++ spec/test_arrays.py
@@ -1,29 +1,29 @@
 import unittest
 from src.arrays import *
 
+
+#helpers
+
+
 class TestArrays(unittest.TestCase):
   
   #every
   def test_1(self):
-    def equals5(val):
-      return val == 5
-    self.assertTrue(every([5,5,5,5], equals5), 'error message here')
+    print('Check every value in array equal 5')
+    self.assertTrue(every([5,5,5,5], lambda val: val == 5 ), 'error message here')
 
   def test_2(self):
-    def equals7(val):
-      return val == 7
-    self.assertFalse(every([5,5,6,7], equals7), 'error message here')
+    print('Check every value in array does not equal 7')
+    self.assertFalse(every([5,5,6,7], lambda val: val == 7 ), 'error message here')
 
   #some
   def test_some1(self):
-    def equals5(val):
-      return val == 5
-    self.assertTrue(some([4,5,6,7], equals5), 'error message here')
+    print('Check some value in array equals 5')
+    self.assertTrue(some([4,5,6,7], lambda val: val == 5 ), 'error message here')
 
   def test_some2(self):
-    def equals7(val):
-      return val == 7
-    self.assertFalse(some([5,5,6,8], equals7), 'error message here')
+    print('Check some value in array does not equal 7')
+    self.assertFalse(some([5,5,6,8], lambda val: val == 7 ), 'error message here')
 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestArrays)
